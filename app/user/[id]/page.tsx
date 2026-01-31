cfbb73448b654d7fd7deb1ae4bee67a9ee42ee5b
Dis-NN-Sup <dis.nn.sup@gmail.com>
2026-01-31 20:28:30 +0900

    Revert Edge Runtime settings for Koyeb deployment

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,7 +1,5 @@
 import UserProfileClient from '@/components/UserProfileClient';
 
-export const runtime = 'edge';
-
 export default async function UserProfile({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     return <UserProfileClient id={id} />;
